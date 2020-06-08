857da274a730dc3db60eb569c3c012ac44461785
MD5047428 <MD5047428@syncgdc2949.syntelorg.com>
2020-06-08 08:46:25 +0530

    First Project

---
+++ src/test/java/com/mystore/online/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
